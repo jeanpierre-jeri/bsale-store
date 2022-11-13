0844f28a20b15411046878449b7f461b7779ebf5
Jean Pierre <jeanpierre.webdev@gmail.com>
2022-11-13 17:32:16 -0500

    change docs url

--- src/swaggerOptions.js
+++ src/swaggerOptions.js
@@ -8,7 +8,7 @@ export const options = {
     },
     servers: [
       {
-        url: 'http://localhost:4000/api'
+        url: 'https://bsale-store-production.up.railway.app/api'
       }
     ]
   },
